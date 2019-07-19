97be9561c01e7dcd6bddd3101c1bb1de19023fe0
Rob Paskin <rjpaskin+git@gmail.com>
2019-07-19 13:58:16 +0100

    Port over helper from original app

--- lib/report_generator/rails.rb
+++ lib/report_generator/rails.rb
@@ -10,5 +10,16 @@ module ReportGenerator
         end
       end
     end
+
+    # Based on https://github.com/rails/webpacker/blob/v3.6.0/lib/webpacker/railtie.rb#L57-L65
+    initializer 'report_generator.helper' do
+      ActiveSupport.on_load :action_controller do
+        ActionController::Base.helper ReportGenerator::Helper
+      end
+
+      ActiveSupport.on_load :action_view do
+        include ReportGenerator::Helper
+      end
+    end
   end
 end
