84953eea94fe5a4dce0594265c62422b0c22df63
Rob Paskin <rjpaskin+git@gmail.com>
2019-07-24 14:39:06 +0100

    Capture result of generator in worker
    Means that any customisation done to the generator and/or download is
    preserved for downstream code.

--- app/workers/report_generator/worker.rb
+++ app/workers/report_generator/worker.rb
@@ -4,10 +4,13 @@ module ReportGenerator
   class Worker
     include Sidekiq::Worker
 
+    attr_reader :download
+
     sidekiq_options retry: false, queue: 'reports'
 
     def perform(report_download_id)
-      ReportGenerator.process(report_download_id)
+      @download = ReportGenerator.process(report_download_id)
+      yield(download) if block_given?
     end
   end
 end
