58b1260c8935adc66351ea4fdbe67ae20f773a92
Rob Paskin <rjpaskin+git@gmail.com>
2019-09-09 13:54:40 +0100

    Add endpoint to download reports via JWT
    Sidesteps issue with S3 URLs signed using temporary credentials (i.e.
    from using an IAM profile) having an expiry time of 6 hours, rather than
    the full 7 days.
    
    Should also make URLs shorter and less likely to be mangled by Outlook
    or email services.

--- app/controllers/report_generator/downloads_controller.rb
+++ app/controllers/report_generator/downloads_controller.rb
@@ -1,5 +1,18 @@
 module ReportGenerator
   class DownloadsController < ReportGenerator.parent_controller
+    # Order is important here - because ExpiredSignature is a subclass
+    # of DecodeError, it should come first, otherwise Rails will always
+    # use the handler for DecodeError
+    rescue_from 'JWT::DecodeError', with: :jwt_invalid
+    rescue_from 'JWT::ExpiredSignature', with: :jwt_expired
+    rescue_from 'ActiveRecord::RecordNotFound', with: :download_not_found
+
+    def show
+      download = ReportGenerator.download_class.from_jwt(params[:token])
+
+      redirect_to download.expiring_link(expires_in: 5.minutes)
+    end
+
     def create
       ReportGenerator.worker_class.perform_async(
         ReportGenerator.download_class.create_from!(report_download_params).id
@@ -20,6 +33,24 @@ module ReportGenerator
       return process_report_download_params(given_params) if respond_to?(:process_report_download_params)
 
       given_params
+    end
+
+    def jwt_expired
+      redirect_with_message(ReportGenerator.config.jwt_expired_message)
+    end
+
+    def jwt_invalid
+      redirect_with_message(ReportGenerator.config.jwt_invalid_message)
+    end
+
+    def download_not_found
+      redirect_with_message(ReportGenerator.config.download_not_found_message)
+    end
+
+    def redirect_with_message(message)
+      location = request.referrer.presence || '/'
+
+      redirect_to location, alert: message
     end
   end
 end
