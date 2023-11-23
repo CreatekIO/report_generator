90c3729045d1fcc311c1d5393a70c30234fff647
Rob Paskin <rjpaskin+git@gmail.com>
2023-11-23 16:04:42 +0000

    Use Ruby 2.6 with Rails 5.2 in dev/test
    Matches what we run for our apps.
    
    Split out common setup code between `docker-compose.yml` and
    `.buildkite/test.sh` into separate file.

--- .buildkite/test.sh
+++ .buildkite/test.sh
@@ -1,14 +1,6 @@
 #!/usr/bin/env sh
 set -eu
 
-gem install bundler -v 2.3.27
-gem update --system 3.2.3
-
-echo "~~~ bundle install"
-bundle install \
-  --jobs "$(getconf _NPROCESSORS_ONLN)" \
-  --retry 2
-
 echo "~~~ Waiting for MySQL"
 until curl -s -o /dev/null "$DB_HOST:3306"; do
   sleep 5
