4be963d3a611ffa70347672b687833309a07e11f
Gill Manning <gill@createk.io>
2024-05-29 10:36:41 +0100

    set ActiveStorage::Current.host if active storage service is local

--- app/concerns/report_generator/active_storage_download_adapter.rb
+++ app/concerns/report_generator/active_storage_download_adapter.rb
@@ -6,6 +6,10 @@ module ReportGenerator::ActiveStorageDownloadAdapter
   end
 
   def expiring_link(expires_in: ReportGenerator::Download::MAX_EXPIRY)
+    if Rails.application.config.active_storage.service == :local
+      ActiveStorage::Current.host = ReportGenerator.config.local_storage_host
+    end
+
     self.report.service_url(expires_in: expires_in.to_i, disposition: "attachment")
   end
 end
