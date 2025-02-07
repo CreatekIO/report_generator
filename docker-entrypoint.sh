c5aff2400c1ee96c5c2aa22f9a4bdceab8adbfa5
Rob Paskin <rjpaskin+git@gmail.com>
2025-02-07 16:12:40 +0000

    Add GH workflow for CI
    We use containers (rather than `setup-ruby`) since that's more
    representative of the environment in which we use the gem, even though
    it's presumably slower.
    
    Changes from Buildkite:
    
    - Use Docker healthcheck rather than waiting ourselves
    - Inline test script steps in workflows, rather than having a separate
      script (since we don't have the wait-for-MySQL part anymore)
    - Add GitHub-Actions-specific output grouping
    - Don't collect test failures via JUnit (it's overkill for the amount of
      tests we have).
    - Only run tests for 'current' Ruby (rather than matrix) - we'll
      reinstate this (likely with the appraisal gem) once we start again on
      Rails upgrades

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,17 @@
 #!/usr/bin/env sh
 set -eu
 
-echo "~~~ update RubyGems and Bundler"
+# https://docs.github.com/en/actions/writing-workflows/choosing-what-your-workflow-does/workflow-commands-for-github-actions#grouping-log-lines
+group() {
+  echo "::group::${1}"
+}
+
+endgroup() {
+  echo "::endgroup::"
+}
+
+group "update RubyGems and Bundler"
+
 if [ "${RUBY_VERSION:-}" = "2.6.10" ]; then
   gem install bundler -v "~> 2.4.22"
   gem update --system 3.4.22 >/dev/null
@@ -10,7 +20,12 @@ else
   gem update --system 3.2.3 >/dev/null
 fi
 
-echo "~~~ bundle install"
+endgroup
+
+group "bundle install"
+
 bundle install \
   --jobs "$(getconf _NPROCESSORS_ONLN)" \
   --retry 2
+
+endgroup
